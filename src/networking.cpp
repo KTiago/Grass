cdd3a261eb4ea483333175a1fcc3be25ecca47bd
KTiago <tiago.kieliger@gmail.com>
2019-04-07 16:56:10 +0200

    Basic code for large file transfer

--- src/networking.cpp
+++ src/networking.cpp
@@ -2,4 +2,131 @@
 // Created by tiago on 25.03.19.
 //
 
+#include <arpa/inet.h>
 #include "networking.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <cstdio>
+#include <algorithm>
+
+#define SOCKET int
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+/*
+ * Code inspired from
+ * https://stackoverflow.com/questions/25634483/send-binary-file-over-tcp-ip-connection
+ * https://www.geeksforgeeks.org/socket-programming-in-cc-handling-multiple-clients-on-server-without-multi-threading/
+ */
+
+using namespace std;
+
+bool senddata(SOCKET sock, void *buf, int buflen)
+{
+    unsigned char *pbuf = (unsigned char *) buf;
+
+    while (buflen > 0)
+    {
+        int num = send(sock, pbuf, buflen, 0);
+        if (num == -1)
+        {
+            return false;
+        }
+
+        pbuf += num;
+        buflen -= num;
+    }
+
+    return true;
+}
+
+bool sendlong(SOCKET sock, long value)
+{
+    value = htonl(value);
+    return senddata(sock, &value, sizeof(value));
+}
+
+bool sendfile(SOCKET sock, FILE *f)
+{
+    fseek(f, 0, SEEK_END);
+    long filesize = ftell(f);
+    rewind(f);
+    if (filesize == EOF)
+        return false;
+    if (!sendlong(sock, filesize))
+        return false;
+    if (filesize > 0)
+    {
+        char buffer[1024];
+        do
+        {
+            size_t num = MIN(filesize, sizeof(buffer));
+            num = fread(buffer, 1, num, f);
+            if (num < 1)
+                return false;
+            if (!senddata(sock, buffer, num))
+                return false;
+            filesize -= num;
+        }
+        while (filesize > 0);
+    }
+    return true;
+}
+
+bool readdata(SOCKET sock, void *buf, int buflen)
+{
+    unsigned char *pbuf = (unsigned char *) buf;
+
+    while (buflen > 0)
+    {
+        int num = recv(sock, pbuf, buflen, 0);
+        if (num == -1)
+        {
+            return false;
+        }
+        else if (num == 0)
+            return false;
+
+        pbuf += num;
+        buflen -= num;
+    }
+
+    return true;
+}
+
+bool readlong(SOCKET sock, long *value)
+{
+    if (!readdata(sock, value, sizeof(value)))
+        return false;
+    *value = ntohl(*value);
+    return true;
+}
+
+bool readfile(SOCKET sock, FILE *f)
+{
+    long filesize;
+    if (!readlong(sock, &filesize))
+        return false;
+    if (filesize > 0)
+    {
+        char buffer[1024];
+        do
+        {
+            int num = MIN(filesize, sizeof(buffer));
+            if (!readdata(sock, buffer, num))
+                return false;
+            int offset = 0;
+            do
+            {
+                size_t written = fwrite(&buffer[offset], 1, num-offset, f);
+                if (written < 1)
+                    return false;
+                offset += written;
+            }
+            while (offset < num);
+            filesize -= num;
+        }
+        while (filesize > 0);
+    }
+    return true;
+}
