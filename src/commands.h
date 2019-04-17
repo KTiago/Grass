070ee0e079d545bdc225e3eb24dbeba2aeb78fcf
Delphine <delphine.peter@epfl.ch>
2019-04-17 15:03:43 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/commands.h
+++ src/commands.h
@@ -15,6 +15,7 @@ int mkdir_cmd(string dir, user usr, string &out);
 int cd_cmd(string dir, user &usr, string &out);
 int ls_cmd(bool authenticated, string &out, string usrLocation);
 int login_cmd(string uname, map<string, string> allowedUsers, user &usr, string &out);
+int logout_cmd(user &usr, string &out);
 int pass_cmd(string psw, map<string, string> allowedUsers, user &usr, string &out);
 int exec(const char* cmd, string &out);
 int ping_cmd(string host, string &out);
