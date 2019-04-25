cadcd774d0daee79d095a55acb5bbd0532bd059c
KTiago <tiago.kieliger@gmail.com>
2019-04-25 17:16:14 +0200

    Updated input sanitization

--- src/commands.cpp
+++ src/commands.cpp
@@ -25,11 +25,17 @@ const string TRANSFER_ERROR = "Error: file transfer failed.\n";
 const set<char> allowedCharacters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
                                      'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
                                      'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-                                     'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '/'};
+                                     'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '/',
+                                     '\"', '{', '}', '/', '-', '_', '!'};
 string escape(string cmd){
     string escaped;
     for(size_t i = 0; i < cmd.size(); ++i) {
-        if (allowedCharacters.find(cmd[i]) != allowedCharacters.end()) escaped += cmd[i];
+        if (cmd[i] == '"') {
+            escaped += '\"';
+        }
+        else{
+            escaped += cmd[i];
+        }
     }
     return "\"" + escaped + "\"";
 }
